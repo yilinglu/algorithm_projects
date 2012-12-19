1ec3305f58efdd664592b787986f4b98a2ab342e
yilinglu <yilingl@gmail.com>
2012-12-19 08:29:55 -0800

    junit

---
+++ projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/BinarySearchTest.java
@@ -0,0 +1,11 @@
+package com.yiling.lu.algorithm;
+
+import junit.framework.TestCase;
+
+public class BinarySearchTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
--- projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/QuickSortTest.java
+++ projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/QuickSortTest.java
@@ -1,9 +1,7 @@
 package com.yiling.lu.algorithm;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
+
 
 public class QuickSortTest {
 
