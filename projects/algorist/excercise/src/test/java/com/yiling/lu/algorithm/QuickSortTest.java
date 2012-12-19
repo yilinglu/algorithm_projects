5cd0e399b8ee936f8802e8d94dd813c3523a121c
erlangr <yilingl@gmail.com>
2012-12-19 10:23:54 -0800

    Add Test case

--- projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/QuickSortTest.java
+++ projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/QuickSortTest.java
@@ -1,12 +1,24 @@
 package com.yiling.lu.algorithm;
 
-import org.junit.Before;
+import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class QuickSortTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
 	@Before
 	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
