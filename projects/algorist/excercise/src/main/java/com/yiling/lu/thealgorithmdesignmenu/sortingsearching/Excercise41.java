d6d25385c287f6d6c4f08715255e665e3df4ad61
erlangr <yilingl@gmail.com>
2012-12-19 16:11:42 -0800

    add

---
+++ projects/algorist/excercise/src/main/java/com/yiling/lu/thealgorithmdesignmenu/sortingsearching/Excercise41.java
@@ -0,0 +1,15 @@
+package com.yiling.lu.thealgorithmdesignmenu.sortingsearching;
+
+/**
+ * The Grinch is given the job of partitioning 2n players into two teams of n
+players each. Each player has a numerical rating that measures how good he/she is
+at the game. He seeks to divide the players as unfairly as possible, so as to create
+the biggest possible talent imbalance between team A and team B. Show how the
+Grinch can do the job in O(n log n) time.
+
+ * @author YLu
+ *
+ */
+public class Excercise41 {
+
+}
