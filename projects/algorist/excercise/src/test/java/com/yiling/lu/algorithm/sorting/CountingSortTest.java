f6f6971c2b570a19d7cae8af431c8aa2728c35a6
yilinglu <yilingl@gmail.com>
2012-12-23 23:43:32 -0800

    refactoring and completing countingsort impl.

--- projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/sorting/CountingSortTest.java
+++ projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/sorting/CountingSortTest.java
@@ -4,11 +4,79 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.yiling.lu.algorithm.util.AssortedUtils;
+
 public class CountingSortTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		int[] input = {100,5,49,585,884,4,3,32,333,34,33,33,100,1,1,0,0,0,5};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
 	}
 
+	@Test
+	public void testSimple() {
+		int[] input = {33,100,1,1,0,0,0,5};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}
+	
+	@Test
+	public void testSimple2() {
+		int[] input = {10, 0};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}	
+	
+	@Test
+	public void testSorted() {
+		int[] input = {0,1,2,3,4,5,6};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}
+	
+	@Test
+	public void testReverseSoted() {
+		int[] input = {10,9,8,7,6,5,4,3,2,1,0};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}	
+	
+	@Test
+	public void testMonotone() {
+		int[] input = {2,2,2,2,2,2};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}
+	
+	@Test
+	public void testMonotone2() {
+		int[] input = {5,5,5,5,5};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}	
+	
+	
+	@Test
+	public void testMonotone3() {
+		int[] input = {0,0,0,0,0};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}	
+	
+	@Test
+	public void testEmpty() {
+		int[] input = {};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}	
+
+	@Test
+	public void testOneElement() {
+		int[] input = {10};
+		int[] output = CountingSort.sort(input);
+		AssortedUtils.verifyOder(output, true);
+	}	
+	
 }
