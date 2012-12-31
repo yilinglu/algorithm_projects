241be1966a8ffa6977b998c00b2c837600602928
yilinglu <yilingl@gmail.com>
2012-12-30 22:59:27 -0800

    character intvalue conversion test.

--- projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/AppTest.java
+++ projects/algorist/excercise/src/test/java/com/yiling/lu/algorithm/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
      */
     public AppTest( String testName )
     {
-        super( testName );
+	        super( testName );
     }
 
     /**
@@ -35,5 +35,25 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+    
+    @org.junit.Test
+    public void testCharConversion(){
+    	// asci value for A is 65
+    	char ch = 'A';
+    	int minusZero = ch - '0';
+    	int plusZero = ch + '0';
+    	int intvalue = ch;
+    	
+    	System.out.println("plusZero " + plusZero);
+    	System.out.println("minusZero " + minusZero);
+    	System.out.println("cast to num " + (int)ch);
+    }
+    
+    @org.junit.Test
+    public void testConvertIntToChar(){
+    	int intvalue = 65;
+    	
+    	System.out.println("char value " + (char)intvalue);
     }
 }
