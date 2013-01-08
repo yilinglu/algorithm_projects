d7be5589853dde50cf0688ee1e23d5b6338d051b
yilinglu <yilingl@gmail.com>
2013-01-08 02:44:04 -0800

    LongestIncreasingSubsequence

---
+++ projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/problem/longestCommonSubstring/LongestIncreasingSubsequence.java
@@ -0,0 +1,9 @@
+package com.yiling.lu.algorithm.problem.longestCommonSubstring;
+
+public class LongestIncreasingSubsequence {
+
+	public static int[] findLIS(int[] a){
+		int[] result = new int[2];
+		return result;
+	}
+}
