2b3ce8cb468aba4f4e8877b4178da5ac4cbd5b39
erlangr <yilingl@gmail.com>
2013-01-29 16:40:20 -0800

    place holders

--- projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/problem/longestLargest/LongestCommonSubstring.java
+++ projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/problem/longestLargest/LongestCommonSubstring.java
@@ -1,5 +1,21 @@
 package com.yiling.lu.algorithm.problem.longestLargest;
 
+/**
+ * Chapter 8: Dynamic Programming
+ * 
+ * 8-3. [4] The longest common substring (not subsequence) of two strings X and
+ * Y is the longest string that appears as a run of consecutive letters in both
+ * strings. For example, the longest common substring of photograph and
+ * tomography is ograph.
+ * 
+ * (a) Let n = |X| and m = |Y |. Give a Θ(nm) dynamic programming algorithm for
+ * longest common substring based on the longest common subsequence/edit
+ * distance algorithm. (b) Give a simpler Θ(nm) algorithm that does not rely on
+ * dynamic programming.
+ * 
+ * @author ylu
+ * 
+ */
 public class LongestCommonSubstring {
 
 }
---
+++ projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/problem/searching/ProximitySearch.java
@@ -0,0 +1,15 @@
+package com.yiling.lu.algorithm.problem.searching;
+
+/**
+ * 4-45. [5] Given a search string of three words, find the smallest snippet of
+ * the document that contains all three of the search words—i.e. , the snippet
+ * with smallest number of words in it. You are given the index positions where
+ * these words in the document, such as word1: (1, 4, 5), word2: (3, 9,
+ * 10), and word3: (2, 6, 15). Each of the lists are in sorted order, as above.
+ * 
+ * @author ylu
+ * 
+ */
+public class ProximitySearch {
+
+}
