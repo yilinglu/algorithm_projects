8583b129818d281318c8ec3bdf393a5fbac2abe9
yilinglu <yilingl@gmail.com>
2013-02-06 16:37:40 -0800

    Initial Graph.

---
+++ projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/graph/GraphNode.java
@@ -0,0 +1,9 @@
+package com.yiling.lu.algorithm.graph;
+
+public class GraphNode {
+	public int y;
+	public int weight = 1;
+
+	public GraphNode next;
+	
+}
--- projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/graph/SimpleGraph.java
+++ projects/algorist/excercise/src/main/java/com/yiling/lu/algorithm/graph/SimpleGraph.java
@@ -1,8 +1,46 @@
 package com.yiling.lu.algorithm.graph;
 
-import java.util.LinkedList;
+import java.util.List;
 
-public interface SimpleGraph<V> {
-	public EdgeNode[] getAdjacencyLists();
+
+public class SimpleGraph {
+	public static final int MAXV = 1000;
+	
+	public GraphNode[] edges = new GraphNode[MAXV];
+	public int[] degree = new int[MAXV];
+	public int nvertices = 0;
+	public int nedges = 0;
+	boolean directed;
+	
+	public void insertEdge(SimpleGraph g, int x, int y, boolean directed){
+		GraphNode p = new GraphNode();
+		p.y = y;
+		edges[x] = p;
+		
+		
+		g.degree[x]++;
+		
+		if(!directed){
+			insertEdge(g, y, x, true);
+		}else{
+			g.nedges++;
+		}
+		
+	}
+	
+	public void printGraph(SimpleGraph g){
+		for(int i=0; i<edges.length; i++){
+			GraphNode p = edges[i];
+			while(p != null){
+				System.out.print(p.y);
+				p = p.next;
+				
+				if(p != null){
+					System.out.print(",");
+				}
+			}
+			System.out.println();
+		}
+	}
 			
 }
